54eece05271cd0aeb751f8aff21f2a85d0cfc57e
John Paul Justice <supermanxxxs@gmail.com>
2019-04-06 18:00:05 -0700

    Commit

--- app/app.js
+++ app/app.js
@@ -1,19 +1,19 @@
 (function () {
     var mainApp = angular.module("app", ['ngRoute']);
 
-    mainApp.config( function ($routeProvider) {
+    mainApp.config(function ($routeProvider) {
         $routeProvider
-            .when('/',{
-                templateUrl:'main.html',
-                controller:'homeController'
-            })
-            .when('/skillstack', {
-                templateUrl: 'skillStack-template.html',
-                controller: 'skillStackController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+        .when('/',{
+            templateUrl:'main.html',
+            controller:'homeController'
+        })
+        .when('/skillstack', {
+            templateUrl: 'skillStack-template.html',
+            controller: 'skillStackController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
     });
 
 
