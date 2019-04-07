8a490d362e7783ef29ce02a756acdd38750eb2a5
John Paul Justice <supermanxxxs@gmail.com>
2019-04-06 18:01:44 -0700

    Added

--- app/app.js
+++ app/app.js
@@ -1,21 +1,6 @@
 (function () {
     var mainApp = angular.module("app", ['ngRoute']);
 
-    mainApp.config(function ($routeProvider) {
-        $routeProvider
-        .when('/',{
-            templateUrl:'main.html',
-            controller:'homeController'
-        })
-        .when('/skillstack', {
-            templateUrl: 'skillStack-template.html',
-            controller: 'skillStackController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-    });
-
-
+   
 });
 
