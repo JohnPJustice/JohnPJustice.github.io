72707395104e025e276394a655d91faf5e6703db
John Paul Justice <supermanxxxs@gmail.com>
2019-04-25 16:29:36 -0700

    Data binding

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -7,7 +7,7 @@
         $scope.navContact = "Contact Me";
         $scope.navProjects = "Skill Stack";
         $scope.navTitle = "Home";
-        $scope.aboutMe = "About Ma"
+        $scope.aboutMe = "About Ma";
         $scope.mainTitle = "Senior Software Engineer / Site Reliability Engineer";
         $scope.mainSummary = "Senior Software Engineer with 8 years of experience in software development for government/healthcare industries. Highly skilled in troubleshooting, development operations, familiarity with the software development life cycle, continuous integration/continuous delivery, devops, software methodologies and use of best practices and adopted patterns.";
         $scope.mainContactMe = "Professional Links";
@@ -15,7 +15,7 @@
         $scope.nautilusName = "Nautilus Inc.";
         $scope.nautilusPostition= "Senior Site Reliability Engineer / Software Engineer";
         $scope.nautilusEmpTime = "August 2017 - Present";
-        $scope.nautilusSummary =  "Primary tasks include automating business operations, standardizing source control utilizing GIT-flow, creatingapplication pipelines for EC2 instances(windows/linux), lambdas,and other various special deployments.Created cloudformation templates to support automations. Setup application monitoring(supporting a kubernetescluster), synthetic testing(browser selenium scripting, api tests, browser tests, endpoint tests), logging,tooling automation, terraform templates, general task automation, and webhook integrations supportingproprietary applications and business operations."
+        $scope.nautilusSummary =  "Primary tasks include automating business operations, standardizing source control utilizing GIT-flow, creatingapplication pipelines for EC2 instances(windows/linux), lambdas,and other various special deployments.Created cloudformation templates to support automations. Setup application monitoring(supporting a kubernetescluster), synthetic testing(browser selenium scripting, api tests, browser tests, endpoint tests), logging,tooling automation, terraform templates, general task automation, and webhook integrations supportingproprietary applications and business operations.";
 
 
  
