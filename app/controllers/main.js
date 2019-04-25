a9233bb88616c0eb194bd5ce8966ab3bde177f9a
John Paul Justice <supermanxxxs@gmail.com>
2019-04-25 16:28:31 -0700

    TEST

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -7,7 +7,7 @@
         $scope.navContact = "Contact Me";
         $scope.navProjects = "Skill Stack";
         $scope.navTitle = "Home";
-        $scope.aboutMe = "About MEA"
+        $scope.aboutMe = "About Ma"
         $scope.mainTitle = "Senior Software Engineer / Site Reliability Engineer";
         $scope.mainSummary = "Senior Software Engineer with 8 years of experience in software development for government/healthcare industries. Highly skilled in troubleshooting, development operations, familiarity with the software development life cycle, continuous integration/continuous delivery, devops, software methodologies and use of best practices and adopted patterns.";
         $scope.mainContactMe = "Professional Links";
