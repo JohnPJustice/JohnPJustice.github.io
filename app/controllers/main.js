f75e07efb736363e755a6c3be29c6010288323c0
John Paul Justice <supermanxxxs@gmail.com>
2019-04-27 22:21:16 -0700

    Added data binding

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -16,6 +16,14 @@
         $scope.nautilusPostition= "Senior Site Reliability Engineer / Software Engineer";
         $scope.nautilusEmpTime = "August 2017 - Present";
         $scope.nautilusSummary =  "Primary tasks include automating business operations, standardizing source control utilizing GIT-flow, creatingapplication pipelines for EC2 instances(windows/linux), lambdas,and other various special deployments.Created cloudformation templates to support automations. Setup application monitoring(supporting a kubernetescluster), synthetic testing(browser selenium scripting, api tests, browser tests, endpoint tests), logging,tooling automation, terraform templates, general task automation, and webhook integrations supportingproprietary applications and business operations.";
+        $scope.capsaName = "Capsa Healthcare";
+        $scope.capsaTitle ="Senior Software Engineer";
+        $scope.capsaSummary = "Research, design, and implement scalable applications for application software, firmware and software interface, application utilities, data configuration, data extraction/retrieval from the ground up. Rapidly prototype new capabilities. Migrated Azure Data server, cloud services, and web applications.";
+        $scope.integraName = "Integra telecom";
+        $scope.integraTitle = "Software Engineer";
+        $scope.integraSummary = "Full stack system engineer responsible for completion of tasks from stories, bugs and other product backlog items/epics. Implemented Deployment builds with unit tests and locked check-ins. Redesigned Customer Website Portal with a focus on responsive web design for optimal mobile capability.";
+        $scope.engilityName = "Engility";
+
 
 
  
