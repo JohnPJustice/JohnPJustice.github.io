585721c0e847f49b20d64b521973bc527c7e7374
John Paul Justice <supermanxxxs@gmail.com>
2019-07-28 23:04:46 -0700

    Added Chart.js and moment.js for chart introduction

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -9,15 +9,15 @@
         $scope.navTitle = "Home";
         $scope.aboutMe = "About Me";
         $scope.mainTitle = "Senior Software Engineer / Site Reliability Engineer";
-        $scope.mainSummary = "Senior Software Engineer with 8 years of experience in software development for government/healthcare industries. Highly skilled in troubleshooting, development operations, familiarity with the software development life cycle, continuous integration/continuous delivery, devops, software methodologies and use of best practices and adopted patterns.";
+        $scope.mainSummary = "Senior Software Engineer with 9 years of experience in software development for government/healthcare industries. Highly skilled in troubleshooting, development operations, familiarity with the software development life cycle, CICD, devops, software methodologies and use of best practices and adopted patterns.";
         $scope.mainContactMe = "Professional Links";
         $scope.mainExperience = "Experience";
         $scope.nautilusName = "Nautilus Inc.";
-        $scope.nautilusPostition= "Senior Site Reliability Engineer / Software Engineer";
+        $scope.nautilusPostition = "Senior Site Reliability Engineer / Software Engineer";
         $scope.nautilusEmpTime = "August 2018 - Present";
-        $scope.nautilusSummary =  "Primary tasks include automating business operations, standardizing source control utilizing GIT-flow, creatingapplication pipelines for EC2 instances(windows/linux), lambdas,and other various special deployments.Created cloudformation templates to support automations. Setup application monitoring(supporting a kubernetescluster), synthetic testing(browser selenium scripting, api tests, browser tests, endpoint tests), logging,tooling automation, terraform templates, general task automation, and webhook integrations supportingproprietary applications and business operations.";
+        $scope.nautilusSummary = "Primary tasks include automating business operations, standardizing source control utilizing GIT-flow, creatingapplication pipelines for EC2 instances(windows/linux), lambdas,and other various special deployments.Created cloudformation templates to support automations. Setup application monitoring(supporting a kubernetescluster), synthetic testing(browser selenium scripting, api tests, browser tests, endpoint tests), logging,tooling automation, terraform templates, general task automation, and webhook integrations supportingproprietary applications and business operations.";
         $scope.capsaName = "Capsa Healthcare";
-        $scope.capsaTitle ="Senior Software Engineer";
+        $scope.capsaTitle = "Senior Software Engineer";
         $scope.capsaSummary = "Research, design, and implement scalable applications for application software, firmware and software interface, application utilities, data configuration, data extraction/retrieval from the ground up. Rapidly prototype new capabilities. Migrated Azure Data server, cloud services, and web applications.";
         $scope.integraName = "Integra telecom";
         $scope.integraTitle = "Software Engineer";
@@ -28,8 +28,65 @@
         $scope.githubLink = "https://github.com/JohnPJustice";
         $scope.githubTitle = "GitHub";
         $scope.linkedInLink = "https://www.linkedin.com/in/john-paul-justice";
-        $scope.linkedInTitle = "LinkedIn";            
+        $scope.linkedInTitle = "LinkedIn";
+        $scope.skillSet= "Skill Set";
+        var skillChart = new Chart(document.getElementById("devops"), {
+            type: 'pie',
+            data: {
+              labels: ["Testing", "Monitoring", "Automation", "Logging", "Coding","Kubernetes"],
+              datasets: [{
+                label: "Years Experience",
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#ff3333"],
+                data: [5,5,9,9,9,1]
+              }]
+            },
+            options: {
+              title: {
+                display: true,
+                text: 'DevOps Skill Set'
+              }
+            }
+        });
+        var skillChart1 = new Chart(document.getElementById("pie-chart1"), {
+            type: 'pie',
+            data: {
+              labels: ["C#", "Python", ".Net", "Azure", "AWS", "Linux"],
+              datasets: [{
+                label: "Years Experience",
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850", "#ff3333"],
+                data: [9,7,9,5,5,3]
+              }]
+            },
+            options: {
+              title: {
+                display: true,
+                text: 'Professional Coding Languages'
+              }
+            }
+        });
+        var skillChart2 = new Chart(document.getElementById("pie-chart2"), {
+            type: 'pie',
+            data: {
+              labels: ["TFS", "GIT", "Agile", "Waterfall/Canban", "CICD", "Devops"],
+              datasets: [{
+                label: "Years Experience",
+                backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850", "#ff3333"],
+                data: [9,9,7,8,7,4]
+              }]
+            },
+            options: {
+              title: {
+                display: true,
+                text: 'Source control/Practiced Methodologies'
+              }
+            }
+        });
 
 
+
+
+        skillChart.render();
+        skillChart1.render();
+        skillChart2.render();
     })
 })();
